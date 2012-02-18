8914709c8cdef86c0fec6aa5f79f7d0d56f87ca6
Lathanael <101@abwesend.de>
2012-02-18 16:19:02 +0100

    gui stuff

---
+++ src/main/java/de/Lathanael/SimpleCalc/gui/Extras/ExtrasLabel.java
@@ -0,0 +1,32 @@
+/*************************************************************************
+ * Copyright (C) 2012 Philippe Leipold
+ *
+ * SimpleCalc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimpleCalc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimpleCalc. If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
+
+package de.Lathanael.SimpleCalc.gui.Extras;
+
+import org.getspout.spoutapi.gui.GenericLabel;
+
+/**
+ * @author Lathanael (aka Philippe Leipold)
+ *
+ */
+public class ExtrasLabel extends GenericLabel {
+
+	public ExtrasLabel(String name) {
+		super(name);
+	}
+}
